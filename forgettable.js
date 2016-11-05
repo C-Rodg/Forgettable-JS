eb395d0a20ec3bcd358420a071870774e7416a9c
Kobaine <rodgersbc@gmail.com>
2016-11-04 23:31:21 -0700

    created select node method

--- forgettable.js
+++ forgettable.js
@@ -5,7 +5,49 @@
 	License: MIT
 */
 
+// get Element method
+function get(selector){
+	// Call get function so `this` refers to get
+	if(!(this instanceof get)){
+		return new get(selector);
+	}
+
+	let elements = document.querySelectorAll(selector);
+	if(elements.length === 1){
+		this.element = elements[0];
+	} else {
+		this.element = elements;
+	}
+	return this;
+};
+
+// Method to select single node
+get.prototype.select = function(node) {
+	if(this.element.length > 0){
+		this.element = this.element[node];
+	}
+	return this;
+};
+
+// Select single node
+
+
+// Extend method
+get.prototype.get = function(url, async, callback){
+	let request = new XMLHttpRequest();
+	request.open('GET', url, async);
+	request.onreadystatechange = function() {
+		console.log(this.status);
+		callback(request.responseText);
+	};
+	request.send();
+	return request;
+};
+
+
+// Don't extend this way -- NEEDS TO BE REMOVED
 const forget = {
+	// AJAX 
 	get : (url, async, callback) => {
 		let request = new XMLHttpRequest();
 		request.open('GET', url, async);
@@ -27,6 +69,15 @@ const forget = {
 		}
 		request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
 		request.send(data);
+		return request;
+	},
+
+	// ANIMATIONS
+
+
+	// UTILITIES
+	removeClass : (element, class) => {
+
 	}
 };
 
